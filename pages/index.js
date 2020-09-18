c9b528c4044ca07ed2316b3be3efa1f00d743540
Alex Singh <alex@alexsingh.com>
2020-09-18 11:03:02 -0300

    Revised homepage

--- pages/index.js
+++ pages/index.js
@@ -2,42 +2,50 @@ import Nav from '../components/nav'
 
 export default function IndexPage() {
   return (
-    <div class="flex flex-col h-doublescreen bg-cover overflow-hidden"
-    style={{backgroundImage: `url(${'infinitedesert-keith-hardy.jpg'})`}}
+    <div className="flex flex-col h-screen bg-cover overflow-hidden"
     >
-      {/* <Nav /> */}
-      <div className='block'>
-      <h1 className="text-header">Infinite</h1>
-      </div>
-      <div className="flex justify-center h-auto py-20">
-        <h2 className="lg:text-4xl sm:text-2xl  text-center">
-          Self-sustaining internet systems for the world to come
+    
+       
+      <div className="flex flex-wrap full-width h-screen items-end z-40">
+        <div className="container-logo inline-block">
+        <h1 className="text-logo block text-6xl">INFINITE</h1>
+        <h1 className="text-logo text-logo-bottom inline-block text-6xl">SCROLL</h1>
+        </div>
+        <ul class="flex flex-grow flex-wrap lg:p-10 p-3 text-2xl ">
+          <li className="flex flex-wrap flex-grow w-full justify-center mb-10 px-3">
+          <h2 className="block text-3xl text-center">
+          Subversive software for an optimistic internet
         </h2>
-      </div>
-      <div class="flex h-screen items-end">
-        <ul class="flex flex-grow flex-wrap p-10 lg:text-3xl sm:text-2xl">
-          <li class="flex flex-grow md:justify-start sm:justify-center mx-10">
+          </li>
+          
+          <li class="flex-grow">
+          <ul class='flex flex-wrap justify-center'>
+          <li class="flex flex-grow  md:mx-10 sm:mx-0 md:justify-start sm:justify-center">
             <a href="mailto:squad@infinitescroll.org">squad@infinitescroll.org
             </a>
             </li>
-          <li class="flex-grow">
-          <ul class='flex justify-around'>
-          <li><a href="https://www.github.com/openworklabs/" target="_blank">GitHub</a></li>
-          <li>
+          <ul className="flex">
+          <li className="mx-2"><a href="https://www.github.com/openworklabs/" target="_blank">GitHub</a></li>
+          <li className="mx-2">
             <a href="https://www.twitter.com/infinitescroll/" target="_blank">Twitter
             </a>
             </li>
-          <li>
+          <li className="mx-2">
             <a href="https://www.are.na/infinitescroll/" target="_blank">
             Are.na
             </a>
           </li>
           </ul>
+          </ul>
           </li>
 
         </ul>
       </div>
-      <h1 className="text-footer center">Scroll</h1>
+      <div className="absolute width-full h-screen">
+      <video autoPlay='true' muted="true" loop="true" preload="auto" class="object-cover width-full h-full z-10 fixed" style={{objectPosition: `21%`}}>
+        <source  src="/infinitescroll.mp4" type="video/mp4" />
+      </video>
+      </div>  
     </div>
   )
 }
