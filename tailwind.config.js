0998fb277e907345903b8bd3dc54e7bcfce718ab
Alex Singh <alex@alexsingh.com>
2020-09-30 12:11:26 -0300

    add custom breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,12 @@ module.exports = {
   },
   purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
   theme: {
+      screens: {
+        'sm': {'min': '640px', 'max': '767px'},
+        'md': {'min': '768px', 'max': '1023px'},
+        'lg': {'min': '1024px', 'max': '1440px'},
+        'xl': {'min': '1441px'},
+      },
     extend: {
       colors: {
         'accent-1': '#333',
