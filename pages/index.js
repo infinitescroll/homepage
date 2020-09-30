eca29614e32247ecc01cb81fa479d6f5c7ab9d1b
Alex Singh <alex@alexsingh.com>
2020-09-30 12:11:19 -0300

    Finesse finesse finesse

--- pages/index.js
+++ pages/index.js
@@ -6,26 +6,26 @@ export default function IndexPage() {
     >
     
        
-      <div className="flex flex-wrap full-width h-screen items-end z-40">
+      <div className="flex flex-wrap xl:w-1/3 lg:w-1/2 w-full h-screen items-end z-40 lg:p-10 p-3 xl:p-12 text-2xl">
         <div className="container-logo inline-block">
-        <h1 className="text-logo block text-6xl">INFINITE</h1>
-        <h1 className="text-logo text-logo-bottom inline-block text-6xl">SCROLL</h1>
+        <h1 className="text-logo block">INFINITE</h1>
+        <h1 className="text-logo text-logo-bottom inline-block">SCROLL</h1>
         </div>
-        <ul class="flex flex-grow flex-wrap lg:p-10 p-3 text-2xl ">
-          <li className="flex flex-wrap flex-grow w-full justify-center mb-10 px-3">
-          <h2 className="block text-3xl text-center">
-          Subversive software for an optimistic internet
+        <ul class="flex flex-col w-full flex-wrap">
+          <li className="flex flex-wrap flex-grow  justify-start mb-4 xl:mb-16">
+          <h2 className="block xl:text-6xl lg:text-3xl text-2xl text-left">
+          Subversive software for optimistic internets
         </h2>
           </li>
           
-          <li class="flex-grow">
-          <ul class='flex flex-wrap justify-center'>
-          <li class="flex flex-grow  md:mx-10 sm:mx-0 md:justify-start sm:justify-center">
-            <a href="mailto:squad@infinitescroll.org">squad@infinitescroll.org
+          <li class="flex-grow max-w-xs">
+          <ul class='flex flex-wrap justify-start xl:text-5xl text-1xl'>
+          <li class="flex flex-grow md:justify-start my-3 xl:my-6">
+            <a href="mailto:squad@infinitescroll.org?Subject=Squaaaa">squad@infinitescroll.org
             </a>
             </li>
           <ul className="flex">
-          <li className="mx-2"><a href="https://www.github.com/openworklabs/" target="_blank">GitHub</a></li>
+          <li className="mr-2"><a href="https://www.github.com/openworklabs/" target="_blank">GitHub</a></li>
           <li className="mx-2">
             <a href="https://www.twitter.com/infinitescroll/" target="_blank">Twitter
             </a>
