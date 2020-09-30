e9e7b639291b66819746fa58922b810700f2bb61
Alex Singh <alex@alexsingh.com>
2020-09-30 16:13:31 -0300

    updates

--- pages/index.js
+++ pages/index.js
@@ -42,8 +42,7 @@ export default function IndexPage() {
         </ul>
       </div>
       <div className="absolute w-full h-screen">
-      <video autoPlay='true' muted="true" loop="true" preload="auto" className="object-cover w-full h-full z-10 fixed" style={{objectPosition: `21%`}}>
-        <picture src="/infinitescroll.jpg" />
+      <video autoPlay='true' muted="true" loop="true" preload="auto" className="object-cover w-full h-full z-10 fixed" style={{objectPosition: `21%`}} poster="/infinitescroll.jpg">
         <source  src="/infinitescroll.mp4" type="video/mp4" />
       </video>
       </div>  
