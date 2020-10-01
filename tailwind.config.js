e30fff3dca5cd5c75467d9892d53818661ec6cb9
Alex Singh <alex@alexsingh.com>
2020-10-01 08:25:21 -0300

    Remove max-width to allow for style cascades

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,20 +2,20 @@ module.exports = {
   future: {
     removeDeprecatedGapUtilities: true,
   },
-  purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
+  purge: ["./components/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}"],
   theme: {
-      screens: {
-        'sm': {'min': '640px', 'max': '767px'},
-        'md': {'min': '768px', 'max': '1023px'},
-        'lg': {'min': '1024px', 'max': '1440px'},
-        'xl': {'min': '1441px'},
-      },
+    screens: {
+      sm: { min: "640px" },
+      md: { min: "768px" },
+      lg: { min: "1024px", max: "1440px" },
+      xl: { min: "1441px" },
+    },
     extend: {
       colors: {
-        'accent-1': '#333',
+        "accent-1": "#333",
       },
     },
   },
   variants: {},
   plugins: [],
-}
+};
