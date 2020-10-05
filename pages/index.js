448f85b656fd22b84c5a35419708d10aa45a0025
Alex Singh <alex@alexsingh.com>
2020-10-05 15:57:24 -0300

    more text-size @ xl updates

--- pages/index.js
+++ pages/index.js
@@ -1,62 +1,62 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 
 export default function IndexPage() {
-  const [timeOfDay, setTimeOfDay] = useState('')
+  const [timeOfDay, setTimeOfDay] = useState("");
   useEffect(() => {
-    const hour = new Date().getHours()
+    const hour = new Date().getHours();
     if (hour < 4 || hour > 18) {
-      setTimeOfDay('night')
+      setTimeOfDay("night");
     } else if (hour >= 4 && hour <= 11) {
-      setTimeOfDay('morning')
+      setTimeOfDay("morning");
     } else {
-      setTimeOfDay('day')
+      setTimeOfDay("day");
     }
-  }, [])
+  }, []);
 
   return (
-    <div className='flex flex-col h-fill-available bg-cover overflow-hidden'>
-      <div className='flex flex-wrap xl:w-1/3 lg:w-1/2 w-full h-screen items-end z-40 xl:p-12 sm:p-10 p-3 text-2xl'>
-        <div className='container-logo inline-block'>
+    <div className="flex flex-col h-fill-available bg-cover overflow-hidden">
+      <div className="flex flex-wrap xl:w-1/3 lg:w-1/2 w-full h-screen items-end z-40 xl:p-12 sm:p-10 p-3 text-2xl">
+        <div className="container-logo inline-block">
           <h1>INFINITE</h1>
-          <h1 className='text-logo-bottom inline-block'>SCROLL</h1>
+          <h1 className="text-logo-bottom inline-block">SCROLL</h1>
         </div>
         <ul
           className={`flex flex-col w-full ${
-            timeOfDay !== 'morning' ? 'text-white' : ''
+            timeOfDay !== "morning" ? "text-white" : ""
           }`}
         >
-          <li className='flex flex-wrap justify-start mb-4 xl:mb-16'>
-            <h2 className='xl:text-4xl sm:text-3xl text-3xl text-left'>
+          <li className="flex flex-wrap justify-start mb-4 xl:mb-16">
+            <h2 className="xl:text-5xl sm:text-3xl text-3xl text-left">
               Subversive software for optimistic internets
             </h2>
           </li>
 
-          <li className='max-w-xs'>
-            <ul className='flex flex-wrap justify-start xl:text-5xl text-1xl'>
-              <li className='flex flex-grow md:justify-start my-3 xl:my-6'>
-                <a href='mailto:squad@infinitescroll.org?Subject=Squaaaa'>
+          <li className="max-w-xs">
+            <ul className="flex flex-wrap justify-start xl:text-4xl text-1xl">
+              <li className="flex flex-grow md:justify-start my-3 xl:my-6">
+                <a href="mailto:squad@infinitescroll.org?Subject=Squaaaa">
                   squad@infinitescroll.org
                 </a>
               </li>
-              <ul className='flex'>
-                <li className='mr-2'>
+              <ul className="flex">
+                <li className="mr-2">
                   <a
-                    href='https://www.github.com/openworklabs/'
-                    target='_blank'
+                    href="https://www.github.com/openworklabs/"
+                    target="_blank"
                   >
                     GitHub
                   </a>
                 </li>
-                <li className='mx-2'>
+                <li className="mx-2">
                   <a
-                    href='https://www.twitter.com/infinitescroll/'
-                    target='_blank'
+                    href="https://www.twitter.com/infinitescroll/"
+                    target="_blank"
                   >
                     Twitter
                   </a>
                 </li>
-                <li className='mx-2'>
-                  <a href='https://www.are.na/infinite-scroll/' target='_blank'>
+                <li className="mx-2">
+                  <a href="https://www.are.na/infinite-scroll/" target="_blank">
                     Are.na
                   </a>
                 </li>
@@ -73,14 +73,14 @@ export default function IndexPage() {
           autoPlay={true}
           muted={true}
           loop={true}
-          preload='auto'
-          className='absolute object-cover w-full h-full z-10 fixed'
+          preload="auto"
+          className="absolute object-cover w-full h-full z-10 fixed"
           style={{ objectPosition: `21%` }}
-          poster={'infinitescroll-' + timeOfDay + '.jpg'}
+          poster={"infinitescroll-" + timeOfDay + ".jpg"}
         >
           <source
-            src={'infinitescroll-' + timeOfDay + '.mp4'}
-            type='video/mp4'
+            src={"infinitescroll-" + timeOfDay + ".mp4"}
+            type="video/mp4"
           />
         </video>
       )}
@@ -113,5 +113,5 @@ export default function IndexPage() {
         <source src="/infinitescroll-night.mp4" type="video/mp4" />
       </video> */}
     </div>
-  )
+  );
 }
